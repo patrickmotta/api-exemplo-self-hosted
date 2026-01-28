ed9f25f479d03ff540bc939b7cf8c7bb962e5fb5
foxFluxy <patrick.ramos.motta@gmail.com>
2026-01-27 23:28:29 -0300

    add index basico

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end('API rodando via systemd');
+});
+
+server.listen(3001, '127.0.0.1', () => {
+  console.log('API na porta 3001');
+});
